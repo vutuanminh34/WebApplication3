e9c3ee458594d03fe6e387d67233317088995585
= <vutuanminh34@gmail.com>
2020-10-19 14:10:39 +0700

    Add project files.

---
+++ WebApplication3/Models/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace WebApplication3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool abc() => true;
+        
+    }
+}
